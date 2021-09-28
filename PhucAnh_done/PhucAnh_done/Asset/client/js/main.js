5e2053c542a1e0365ee363308366765fe5f165e5
thelong2212 <78224213+thelong2212@users.noreply.github.com>
2021-09-28 11:15:28 +0700

    ..

--- PhucAnh_done/PhucAnh_done/Asset/client/js/main.js
+++ PhucAnh_done/PhucAnh_done/Asset/client/js/main.js
@@ -2,8 +2,8 @@
     $('.your-class').slick({
         dots: true,
         infinite: true,
-        speed: 500,
         fade: true,
+        cssEase: 'linear',
         autoplay: true,
         autoplaySpeed: 2000,
     });
